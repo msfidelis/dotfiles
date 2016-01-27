2e356518d513f0f10e1c351544dfd6ff43ef94fd
Matheus Fidelis <msfidelis@outlook.com>
2016-01-27 16:58:49 -0200

    Spotify libcrypt file

--- makepc.sh
+++ makepc.sh
@@ -149,7 +149,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB31
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install spotify-client
-
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_amd64.deb
+dpkg -i libgcrypt11_1.5.0-5+deb7u3_amd64.deb
 
 echo ''
 echo -e "${Bgre}[*] CONFIGURANDO O AMBIENTE DE DESENVOLVIMENTO WEB - LAMP"
