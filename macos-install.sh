2f9bccff0d242f70ccb16ee7ed35d12821c91a38
Matheus Fidelis <msfidelis@users.noreply.github.com>
2016-11-15 23:12:06 -0200

    Progressive run

--- macos-install.sh
+++ macos-install.sh
@@ -5,21 +5,21 @@ sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.pl
 
 #Instala o Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-brew update
-brew upgrade
+brew update;
+brew upgrade;
 
 #Instala alguns componentes de dev/essenciais de primeiro momento
-brew install git nmap 
+brew install git nmap ;
 
 #Instala o PHP7
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap homebrew/homebrew-php
-brew install php70
+brew tap homebrew/dupes;
+brew tap homebrew/versions;
+brew tap homebrew/homebrew-ph;p
+brew install php70;
 
 #Instala o Apache
-brew tap homebrew/apache
-sudo apachectl stop
+brew tap homebrew/apache;
+sudo apachectl stop;
 sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
 brew install httpd24 --with-privileged-ports --with-http2
 sudo cp -v /usr/local/Cellar/httpd24/2.4.23_2/homebrew.mxcl.httpd24.plist /Library/LaunchDaemons
@@ -45,14 +45,14 @@ omf install bobthefish;
 #Instala as fontes adicionais 
 git clone https://github.com/powerline/fonts.git;
 cd fonts/
-./install.sh  
+./install.sh;  
 cd ..
 
 #Instala o Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
+php composer-setup.php;
+php -r "unlink('composer-setup.php');";
 
 #Move minhas dotfiles para minha home
 cp -r .vim* /Users/matheus/
