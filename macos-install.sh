d490703fb3e27b1ae73789338a375b68591d18d5
Matheus Fidelis <msfidelis@users.noreply.github.com>
2016-11-15 23:15:40 -0200

    Update macos-install.sh

--- macos-install.sh
+++ macos-install.sh
@@ -53,6 +53,8 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
 php composer-setup.php;
 php -r "unlink('composer-setup.php');";
+sudo mv composer.phar /usr/local/bin;
+sudo chmod +x /usr/local/bin;
 
 #Move minhas dotfiles para minha home
 cp -r .vim* /Users/matheus/
