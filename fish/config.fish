389974dd6a725a9502566c293f31e24733206043
Matheus Fidelis <msfidelis01@gmail.com>
2018-02-22 00:24:59 -0300

    fish

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+ Load fishmarks (http://github.com/techwizrd/fishmarks)
+. $HOME/.fishmarks/marks.fish
+
+#ALIASES
+alias hcat='pygmentize -f terminal256 -O style=monokai -g'
+alias la='ls -lha -S'
+
+#CONFIGS
+set -U EDITOR vim
